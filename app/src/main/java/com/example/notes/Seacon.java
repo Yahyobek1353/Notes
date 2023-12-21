05b9918c077c447549d182e83fb06f70c0db5b67
muxammadaziz-avazhanov <acoroziev297@gmail.com>
2023-12-21 16:45:25 +0600

    design and code

--- app/src/main/java/com/example/notes/Seacon.java
+++ app/src/main/java/com/example/notes/Seacon.java
@@ -1,14 +1,46 @@
 package com.example.notes;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 public class Seacon extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seacon);
+        imageView = findViewById(R.id.img_add);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            if (selectedImageUri != null) {
+                Cursor cursor = getContentResolver().query(selectedImageUri, filePathColumn, null, null, null);
+                if (cursor != null) {
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String imagePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
     }
 }
